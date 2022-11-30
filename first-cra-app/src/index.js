18c4a1dbadccbb032f516bcc6bb26b285c1786e4
RashidCh <114673388+RashidCHAB@users.noreply.github.com>
2022-11-30 15:57:27 +0300

    Delete first-cra-app directory

--- first-cra-app/src/index.js
+++
@@ -1,7 +0,0 @@
-const ReactDOM = require('react-dom/client')
-const {default: App} = require('./components/App')
-
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
-    <App />
-)
